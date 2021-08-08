393b87a12c759e6a0b9d98768f67f8f188a24f1b
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2021-08-08 01:22:12 +0100

    mmmmmmmmmmmmmmmmmmm yes

--- Source/WNDSock.h
+++ Source/WNDSock.h
@@ -12,38 +12,8 @@
 #include <iphlpapi.h>
 #pragma comment(lib, "Ws2_32.lib")
 
-class WNDSock
-{
-	private:
-		//Socket Setup Information.
-		SOCKET Conn = NULL;
-		
-		//Socket State Information.
-		unsigned char CurrentType = 0;
-		unsigned char ExSocketCount = 0;
-		SOCKET* ExSocket = NULL;
-
-	public:
-
-		WNDSock();
-		
-		unsigned char Host(const char* Address, unsigned short Port, unsigned int Backlog);
-		
-		unsigned char Listen(unsigned int Backlog, unsigned char Count);
-
-		unsigned char Join(const char* Address, unsigned short Port);
-
-		unsigned char Send(char* Buffer, unsigned int BufferSize);
-
-		unsigned char Send(char* Buffer, unsigned int BufferSize, unsigned char Index);
-
-		char* Recieve(unsigned int* Size);
-		
-		~WNDSock();
-
-};
-
 #elif __unix__
+
 #ifndef STD
 #define STD
 
@@ -52,10 +22,36 @@ class WNDSock
 #endif
 
 #include <sys/socket.h>
-class WNDSock
-{
-	private:
-		unsigned int xd;
-};
 
 #endif
+
+class WNDSock
+{
+private:
+	//Socket Setup Information.
+	SOCKET Conn = NULL;
+
+	//Socket State Information.
+	unsigned char CurrentType = 0;
+	unsigned char ExSocketCount = 0;
+	SOCKET* ExSocket = NULL;
+
+public:
+
+	WNDSock();
+
+	unsigned char Host(const char* Address, unsigned short Port, unsigned int Backlog);
+
+	unsigned char Listen(unsigned int Backlog, unsigned char Count);
+
+	unsigned char Join(const char* Address, unsigned short Port);
+
+	unsigned char Send(char* Buffer, unsigned int BufferSize);
+
+	unsigned char Send(char* Buffer, unsigned int BufferSize, unsigned char Index);
+
+	char* Recieve(unsigned int* Size);
+
+	~WNDSock();
+
+};
