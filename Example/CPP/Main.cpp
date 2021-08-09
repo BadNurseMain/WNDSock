5b9bdff4dfb9b309961ffa2e5083e14f4247a8e0
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2021-08-09 10:03:21 +0100

    Update Main.cpp

--- Example/CPP/Main.cpp
+++ Example/CPP/Main.cpp
@@ -1,23 +1,32 @@
 #include "WNDSock.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-//Works for Linux and Windows, will only demonstrate what works 
-//seamlessly with both Linux and Windows.
+//Working for both Linux and Windows right now.
 int main()
 {
     int x = 0;
     scanf("%d", &x);
+
+    char* Buff = (char*)malloc(10);
+    strcpy(Buff, "Howdyfela");
+
     if(x == 1)
     {
         WNDSock x = WNDSock();
         if(x.Host("127.0.0.1", 7209)) printf("Error hosting! \n");
         x.Listen(1,1);
+        x.Send(Buff, 10);
     }
     else
     {
         WNDSock y = WNDSock();
         if(y.Join("127.0.0.1", 7209)) printf("Error joining! \n");
+        unsigned int Size = 0;
+        char* Buff = y.Recieve(&Size);
+
+        printf("%s", Buff);
     }
 
     return 0;
