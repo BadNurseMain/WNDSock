5bc86fa3129590a55707cc90ede7aa0cfb3b7878
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2021-08-09 10:51:02 +0100

    Update Main.cpp

--- Example/CPP/Main.cpp
+++ Example/CPP/Main.cpp
@@ -18,16 +18,17 @@ int main()
         if(x.Host("127.0.0.1", 7209)) printf("Error hosting! \n");
         x.Listen(1,1);
         x.Send(Buff, 10);
+        free(Buff);
     }
     else
     {
         WNDSock y = WNDSock();
         if(y.Join("127.0.0.1", 7209)) printf("Error joining! \n");
         unsigned int Size = 0;
-        char* Buff = y.Recieve(&Size);
+        char* Buff2 = y.Recieve(&Size);
 
         printf("%s", Buff);
-        free(Buff);
+        free(Buff2);
     }
 
     return 0;
