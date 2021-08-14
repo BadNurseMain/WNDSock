7c570d52b63aa089713e326037493da1247ac170
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2021-08-14 19:44:41 +0100

    Update Main.c

--- Example/C/Main.c
+++ Example/C/Main.c
@@ -1,13 +1,27 @@
-#include "WNDSock.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "WNDSock.h"
 
+//Works for Windows as of right now.
 int main()
 {
-	WNDSOCK x = createSocket();
-	WNDSOCK* Clients = hostSocket(x, "192.168.1.1", 7209, 20);
-	if (!Clients) return 1;
+	int Res = 0;
+	scanf_s("%d", &Res);
 
-	free(Clients);
+	if(Res == 1)
+	{
+		WNDSOCK x = createSocket();
+		WNDSOCK* Clients = hostSocket(x, "192.168.1.1", 7209, 1);
+		if (!Clients) return 1;
+
+		free(Clients);
+		closeSocket(x);
+	} else 
+	{
+		WNDSOCK x = createSocket();
+		if (joinSocket(x, "192.168.1.1", 7209)) return 1;
+		closeSocket(x);
+	}
+	
 	return 0;
 }
