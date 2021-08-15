34ed6dbbe9b453d8eda65b026e5bb1dc17e884c3
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2021-08-15 17:44:34 +0100

    Update Main.c

--- Example/C/Main.c
+++ Example/C/Main.c
@@ -12,14 +12,27 @@ int main()
 	{
 		WNDSOCK x = createSocket();
 		WNDSOCK* Clients = hostSocket(x, "192.168.1.1", 7209, 1);
+		
 		if (!Clients) return 1;
 
+		const char* Buffer = "HowdyGamer\0";
+		
+		if(sendData(Clients[0], Buffer, strlen(Buffer) + 1) == WND_ERROR) return 2;
+		
 		free(Clients);
 		closeSocket(x);
 	} else 
 	{
 		WNDSOCK x = createSocket();
+		
 		if (joinSocket(x, "192.168.1.1", 7209)) return 1;
+		
+		unsigned int Total Size = 0;
+		char* Buffer = receieveData(x, &TotalSize);
+		
+		if(!Buffer) return 2;
+		
+		printf("%s \n", Buffer);
 		closeSocket(x);
 	}
 	
