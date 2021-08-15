06846cbb064165dfa776c402c7f33a2544459576
Chloe Moran <80408197+BadNurseMain@users.noreply.github.com>
2021-08-15 14:27:29 +0100

    Update WNDSock.c

--- Source/C/WNDSock.c
+++ Source/C/WNDSock.c
@@ -30,15 +30,15 @@ WNDSOCK createSocket()
 
 WNDSOCK* hostSocket(WNDSOCK Socket, const char* IP, unsigned short Port, unsigned char ClientCount)
 {
+	if (!ClientCount) return NULL;
+
 	WNDSOCKADDR Addr = { 0 };
 	Addr.sin_addr.S_un.S_addr = WND_STRTOIP(IP);
 	Addr.sin_port = Port;
 	Addr.sin_family = WND_IPV4;
 	
 	if(bind(Socket, (const struct sockaddr*)&Addr, sizeof(Addr))) return NULL;
-	listen(Socket, (int)~0);
-
-	if (!ClientCount) return NULL;
+	listen(Socket, ClientCount);
 
 	WNDSOCK* Ptr = malloc(sizeof(WNDSOCK) * ClientCount);
 	if (!Ptr) return NULL;
@@ -69,6 +69,72 @@ unsigned char joinSocket(WNDSOCK Socket, const char* IP, unsigned short Port)
 	return 0;
 }
 
+void closeSocket(WNDSOCK Socket)
+{
+	closesocket(Socket);
+	if (!(--TOTALSOCKCOUNT)) WSACleanup();
+	return;
+}
+
+#elif __unix__
+#define WND_IPV4 AF_INET
+#define WND_STREAM SOCK_STREAM
+#define WND_TCP IPPROTO_TCP
+#define WNDSOCK_INVAL -1
+
+#define WND_STRTOIP(IP) inet_addr(IP);
+
+//Linux Specific Functions.
+WNDSOCK createSocket()
+{
+    int Conn = socket(WND_IPV4, WND_STREAM, WND_TCP);
+    if(Conn == -1) return WND_ERROR;
+
+    return Conn;    
+}
+
+void closeSocket(WNDSOCK Socket)
+{
+    close(Socket);
+    return;
+}
+
+unsigned char joinSocket(WNDSOCK Socket, const char* IP, unsigned short Port)
+{
+    WNDSOCKADDR Addr = {0};
+    Addr.sin_family = WND_IPV4;
+    Addr.sin_port = Port;
+    Addr.sin_addr.s_addr = WND_STRTOIP(IP);
+
+    if(connect(Socket, (const struct sockaddr*)&Addr, sizeof(Addr))) return WND_ERROR;
+    return 0;
+}
+
+WNDSOCK* hostSocket(WNDSOCK Socket, const char* IP, unsigned short Port, unsigned char ClientCount)
+{
+    if(!ClientCount) return NULL;
+
+    WNDSOCKADDR Addr = {0};
+    Addr.sin_family = WND_IPV4;
+    Addr.sin_port = Port;
+    Addr.sin_addr.s_addr = WND_STRTOIP(IP);
+
+    if(listen(Socket, ClientCount)) return NULL;
+    
+    WNDSOCK* Ptr = malloc(sizeof(WNDSOCK)*ClientCount);
+
+    for(unsigned char x = 0; x < ClientCount; x++)
+    {
+        Ptr[x] = accept(Socket, NULL, NULL);
+        if(Ptr[x] == WNDSOCK_INVAL) return NULL;        
+    }    
+
+    return Ptr;
+}
+
+#endif
+
+//Universal Functions (Written Exactly the Same for Both Windows and Linux)
 unsigned char sendData(WNDSOCK Socket, const char* Data, unsigned int Size)
 {
 	if(send(Socket, (const char*)&Size, sizeof(Size), 0) != sizeof(Size)) return WND_ERROR;
@@ -78,7 +144,7 @@ unsigned char sendData(WNDSOCK Socket, const char* Data, unsigned int Size)
 
 char* recieveData(WNDSOCK Socket, unsigned int* Size)
 {
-	if(recv(Socket, (char*)&Size, sizeof(*Size), 0) != sizeof(*Size)) return NULL;
+	if(recv(Socket, (char*)Size, sizeof(*Size), 0) != sizeof(*Size)) return NULL;
 
 	char* Ptr = malloc(*Size);
 	if (!Ptr) return NULL;
@@ -86,15 +152,3 @@ char* recieveData(WNDSOCK Socket, unsigned int* Size)
 	if (recv(Socket, Ptr, *Size, 0) != *Size) return NULL;
 	return Ptr;
 }
-
-void closeSocket(WNDSOCK Socket)
-{
-	closesocket(Socket);
-	if (!(--TOTALSOCKCOUNT)) WSACleanup();
-	return;
-}
-
-#elif __unix__
-
-
-#endif
